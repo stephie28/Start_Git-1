f57101328d253e1169393a67a53e727b654687a8
NoaYakobson <116171593+NoaYakobson@users.noreply.github.com>
2022-10-19 16:23:08 +0300

    Add files via upload

---
+++ ExmpleGit.py
@@ -0,0 +1,14 @@
+#1
+print(" Hello Enter your  name: ")
+
+#2
+print(" Hello Enter your adress: ")
+name = "Noa"
+address = "Vaizman Holon"
+print("my name is:" + name + "my address is:" + address)
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
